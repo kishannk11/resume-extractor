5aaf61f5a0a8d42a7495d056b256646ef3f52a76
Kishan Nayak <kishannk11@gmail.com>
2023-11-22 01:18:25 +0530

    Final Commit with UI Changes

---
+++ resume-extractor/dashboard/index.php
@@ -0,0 +1,110 @@
+<?php
+require('navbar.php')
+?>
+
+        <div class="page-wrapper">
+
+            <!-- Page Content-->
+            <div class="page-content">
+
+                <div class="container-fluid">
+                    <!-- Page-Title -->
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="page-title-box">
+                                <div class="float-right">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="javascript:void(0);">Resume Extractor</a></li>
+                                        <li class="breadcrumb-item active">Dashboard</li>
+                                    </ol>
+                                </div>
+                                <h4 class="page-title">Dashboard</h4>
+                            </div><!--end page-title-box-->
+                        </div><!--end col-->
+                    </div>
+                    <!-- end page title end breadcrumb -->
+                    <div class="row">
+                        <div class="col-lg-12">  
+                            <div class="row">
+                                <div class="col-lg-3">
+                                    <div class="card dash-data-card text-center">
+                                        <div class="card-body"> 
+                                            <div class="icon-info mb-3">
+                                                <i class="fas fa-ticket-alt bg-soft-warning"></i>
+                                            </div>
+                                            <h3 class="text-dark">184</h3>
+                                            <h6 class="font-14 text-dark">New Tickets</h6>                                                                                                                            
+                                        </div><!--end card-body--> 
+                                    </div><!--end card-->   
+                                </div><!-- end col-->
+                                <div class="col-lg-3">
+                                    <div class="card dash-data-card text-center">
+                                        <div class="card-body"> 
+                                            <div class="icon-info mb-3">
+                                                <i class="fab fa-codepen bg-soft-pink"></i>
+                                            </div>
+                                            <h3 class="text-dark">101</h3>
+                                            <h6 class="font-14 text-dark">Open Tickets</h6>                                                                                                                            
+                                        </div><!--end card-body--> 
+                                    </div><!--end card-->   
+                                </div><!-- end col-->  
+                                <div class="col-lg-3">
+                                    <div class="card dash-data-card text-center">
+                                        <div class="card-body"> 
+                                            <div class="icon-info mb-3">
+                                                <i class="fas fa-hands bg-soft-success"></i>
+                                            </div>
+                                            <h3 class="text-dark">18</h3>
+                                            <h6 class="font-14 text-dark">On Hold</h6>                                                                                                                            
+                                        </div><!--end card-body--> 
+                                    </div><!--end card-->   
+                                </div><!-- end col-->
+                                <div class="col-lg-3">
+                                    <div class="card dash-data-card text-center">
+                                        <div class="card-body"> 
+                                            <div class="icon-info mb-3">
+                                                <i class="fas fa-lock bg-soft-primary"></i>
+                                            </div>
+                                            <h3 class="text-danger">92</h3>
+                                            <h6 class="font-14 text-dark">Unassigned</h6>                                                                                                                            
+                                        </div><!--end card-body--> 
+                                    </div><!--end card-->   
+                                </div><!-- end col-->                       
+                            </div><!--end row-->                                                                        
+                        </div><!--end col-->
+                        
+                    </div><!--end row-->
+                    
+                    
+
+                </div><!-- container -->
+
+                <footer class="footer text-center text-sm-left">
+                    &copy; Resume Extractor<span class="text-muted d-none d-sm-inline-block float-right">Crafted by <i class="mdi mdi-heart text-danger"></i> by Kishan Nayak</span>
+                </footer><!--end footer-->
+            </div>
+            <!-- end page content -->
+        </div>
+        <!-- end page-wrapper -->
+
+        
+
+
+        <!-- jQuery  -->
+        <script src="../assets/js/jquery.min.js"></script>
+        <script src="../assets/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/js/metismenu.min.js"></script>
+        <script src="../assets/js/waves.js"></script>
+        <script src="../assets/js/feather.min.js"></script>
+        <script src="../assets/js/jquery.slimscroll.min.js"></script>
+        <script src="../assets/js/jquery-ui.min.js"></script>
+
+        <script src="../../plugins/apexcharts/apexcharts.min.js"></script>
+        <script src="../assets/pages/jquery.helpdesk-dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="../assets/js/app.js"></script>
+        
+    </body>
+
+</html>
---
+++ resume-extractor/dashboard/navbar.php
@@ -0,0 +1,100 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Resume Extractor</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="../assets/images/favicon.ico">
+        <link href="../../plugins/dropify/css/dropify.min.css" rel="stylesheet">
+
+        <!-- Responsive datatable examples -->
+        <link href="../../plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- App css -->
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../assets/css/jquery-ui.min.css" rel="stylesheet">
+        <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="../assets/css/metisMenu.min.css" rel="stylesheet" type="text/css" />
+        <link href="../assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+        
+         <!-- Top Bar Start -->
+         <div class="topbar">
+
+            <!-- LOGO -->
+            <div class="topbar-left">
+                <a href="../dashboard/crm-index.html" class="logo">
+                    <span>
+                        <img src="../assets/images/logo-sm.png" alt="logo-small" class="logo-sm">
+                    </span>
+                    <span>
+                        <img src="../assets/images/logo.png" alt="logo-large" class="logo-lg logo-light">
+                        <img src="../assets/images/logo-dark.png" alt="logo-large" class="logo-lg">
+                    </span>
+                </a>
+            </div>
+            <!--end logo-->
+            <!-- Navbar -->
+            <nav class="navbar-custom">    
+                <ul class="list-unstyled topbar-nav float-right mb-0"> 
+
+
+                    <li class="dropdown">
+                        <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button"
+                            aria-haspopup="false" aria-expanded="false">
+                            <img src="../assets/images/users/user-1.png" alt="profile-user" class="rounded-circle" /> 
+                            <span class="ml-1 nav-user-name hidden-sm">Admin <i class="mdi mdi-chevron-down"></i> </span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="#"><i class="ti-user text-muted mr-2"></i> Profile</a>
+                            <!-- <a class="dropdown-item" href="#"><i class="ti-wallet text-muted mr-2"></i> My Wallet</a> -->
+                            <!-- <a class="dropdown-item" href="#"><i class="ti-settings text-muted mr-2"></i> Settings</a> -->
+                            <!-- <a class="dropdown-item" href="#"><i class="ti-lock text-muted mr-2"></i> Lock screen</a> -->
+                            <div class="dropdown-divider mb-0"></div>
+                            <a class="dropdown-item" href="#"><i class="ti-power-off text-muted mr-2"></i> Logout</a>
+                        </div>
+                    </li>
+                </ul><!--end topbar-nav-->
+    
+                <ul class="list-unstyled topbar-nav mb-0">                        
+                    <li>
+                        <button class="nav-link button-menu-mobile waves-effect waves-light">
+                            <i class="ti-menu nav-icon"></i>
+                        </button>
+                    </li>
+                    <li class="hide-phone app-search">
+                    </li>
+                </ul>
+            </nav>
+            <!-- end navbar-->
+        </div>
+        <!-- Top Bar End -->
+
+        
+        <!-- Left Sidenav -->
+        <div class="left-sidenav">
+            <ul class="metismenu left-sidenav-menu">
+                <li>
+                    <a href="javascript: void(0);"><i class="ti-bar-chart"></i><span>Dashboard</span><span class="menu-arrow"><i class="mdi mdi-chevron-right"></i></span></a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li class="nav-item"><a class="nav-link" href="index.php"><i class="ti-control-record"></i>Dashboard</a></li>
+                        <li class="nav-item"><a class="nav-link" href="upload.php"><i class="ti-control-record"></i>Upload Resume</a></li>
+                        <li class="nav-item"><a class="nav-link" href="candidates.php"><i class="ti-control-record"></i>Candidates</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <!-- end left-sidenav-->
